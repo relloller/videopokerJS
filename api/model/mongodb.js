2fae8045c27616f8e7f125f6022aca5938d0cd0e
Victor Shaw <relloller@users.noreply.github.com>
2017-04-05 05:56:56 -0700

    Update mongodb.js

--- api/model/mongodb.js
+++ api/model/mongodb.js
@@ -1,21 +1,14 @@
 //  ./api/model/mongodb.js
-var options = {
-  db: { native_parser: true },
-  // server: { poolSize: 5 },
-  // replset: { rs_name: 'myReplicaSetName' },
-  user: 'vegascodesdev',
-  pass: 'v3g@sc0d3s'
-}
 // mongoose.connect(uri, options);
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/vegascodesdev', options);
+mongoose.connect('mongodb://localhost/videopokerjs');
 mongoose.connection.on('connected', function() {
-    console.log('connected vegascodes db');
+    console.log('connected videopokerjs db');
 });
 
 process.on('SIGINT', function() {
     mongoose.connection.close(function() {
-        console.log('disconnected vegascodes db');
+        console.log('disconnected videopokerjs db');
         process.exit(0);
     });
 });
